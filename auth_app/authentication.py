13f56909a1a6cf42ffdd28f6ea32586842356ce6
Mr. EnglishCat <mr.englishcat@gmail.com>
2025-09-18 22:49:10 +0300

    Доработки
    Добавлен кастомный JWTAuthentication класс

--- auth_app/authentication.py
+++ auth_app/authentication.py
@@ -1,27 +1,36 @@
-from django.contrib.auth.backends import BaseBackend
-from rest_framework.authtoken.admin import User
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+
+from auth_app.models import CustomUserModel
+from auth_app.utils import decode_token
 
 
-class EmailBackend(BaseBackend):
+
+class JWTAuthentication(BaseAuthentication):
     def authenticate(self, request):
+        auth_header = request.headers.get('Authorization')
+        if not auth_header:
+            return None
 
-        print("HERE", request.POST.get('email'))
-        email = request.POST.get('email')
-        if email:
-            try:
-                user = User.objects.get(email=email)
-                if user.is_active:
-                    return user
-                else:
-                    return None
-            except User.DoesNotExist:
-                return None
+        if not auth_header.startswith('Bearer '):
+            raise AuthenticationFailed('Невалидный JWT токен!')
 
+        token = auth_header.split()[-1]
 
+        payload = decode_token(token)
 
-    def get_user(self, user_id):
+        if not payload:
+            raise AuthenticationFailed('Невалидный или истёкший токен!')
+
 
         try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
+            user = CustomUserModel.objects.get(pk=payload, is_active=True)
+        except CustomUserModel.DoesNotExist:
+            raise AuthenticationFailed('Пользователь не найден!')
+        else:
+            return user, token
+
+
+
+    def authenticate_header(self, request):
+        return f'Bearer realm="api"'
