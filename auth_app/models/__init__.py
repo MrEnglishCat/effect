307e3b2482f1fb941e658123b29ffb8f7cc42f4a
Mr. EnglishCat <mr.englishcat@gmail.com>
2025-09-18 22:17:10 +0300

    Доработки Добавлен импорт модели сессий

--- auth_app/models/__init__.py
+++ auth_app/models/__init__.py
@@ -3,3 +3,4 @@ from .roles import RolesModel
 from .actions import ActionsModel
 from .resourses import ResoursesModel
 from .permissions import PermissionsModel
+from .sessions import SessionsModel
