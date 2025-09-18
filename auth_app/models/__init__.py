99735ec14d2fe7c5a2e0c462e7aaae300082775c
Mr. EnglishCat <mr.englishcat@gmail.com>
2025-09-19 00:05:23 +0300

    Добавлена модель для хранения отозванных токенов

--- auth_app/models/__init__.py
+++ auth_app/models/__init__.py
@@ -4,3 +4,4 @@ from .actions import ActionsModel
 from .resourses import ResoursesModel
 from .permissions import PermissionsModel
 from .sessions import SessionsModel
+from .blacklist_tokens import BlacklistToken
---
+++ auth_app/models/blacklist_tokens.py
@@ -0,0 +1,24 @@
+import uuid
+
+from django.db import models
+
+
+
+class BlacklistToken(models.Model):
+
+    jti = models.UUIDField("JTI токена", editable=False, db_index=True)
+    user = models.ForeignKey('auth_app.CustomUserModel', on_delete=models.CASCADE, related_name='blacklist_tokens')
+    blacklist_at = models.DateTimeField("Дата отзыва токена", auto_now_add=True)
+    expires_at = models.DateTimeField("Срок действия токена")
+
+
+    def __str__(self):
+        return self.jti
+
+
+    class Meta:
+        verbose_name = 'Отозванный токен'
+        verbose_name_plural = 'Отозванные токены'
+        unique_together = (('user', 'jti'),)
+
+
