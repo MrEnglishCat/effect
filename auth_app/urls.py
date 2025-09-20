1470cfa99b253c1c1786bff20da27ca45632e1cc
Mr. EnglishCat <mr.englishcat@gmail.com>
2025-09-20 17:42:50 +0300

    Оптимизация
    Оптимизирован импорт из views
    
    Добавлены эндпоинты для получения сессий для юзера и админа

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-# from auth_app.views import CustomUserAPIView, LoginAPIView, LogoutAPIView, RegisterAPIView, \
-#     TokenRevokeAPIView, TokenRevokeALLAPIView, AdminTokenRevokeALLAPIView, MyProfileAPIView, RefreshTokenAPIView
-from auth_app.views import LoginAPIView, CustomUserAPIView, MyProfileAPIView, RegisterAPIView, LogoutAPIView, \
-    RefreshTokenAPIView, TokenRevokeAPIView, TokenRevokeALLAPIView, AdminTokenRevokeALLAPIView
+from auth_app.views import *
 
 user_router = routers.DefaultRouter()
 user_router.register(r'users', CustomUserAPIView, basename='user')
@@ -25,7 +22,9 @@ urlpatterns = [
     path('token/revoke_all/<int:user_id>/', AdminTokenRevokeALLAPIView.as_view(), name='admin_token_revoke_all'),
 
     # TODO нужен эндпоинт для получения списка активных сессий(одна сессия одна запись в issueJWT
+    # TODO нужен эндпоинт для АДМИНОВ, ЧТО БЫ ПОСМОТРЕТЬ СЕССИИ ВСЕХ ПОЛЬЗОВАТЕЛЕЙполучения списка активных сессий(одна сессия одна запись в issueJWT
     path('me/get_sessions/', MySessionsAPIView.as_view(), name='get_my_sessions'),
+    path('me/get_sessions/<int:user_id>', AdminSessionsAPIView.as_view(), name='get_my_sessions'),
     path('me/', MyProfileAPIView.as_view({'get': 'list'}), name='me'),
 
     path('', include(user_router.urls), name='users'),
