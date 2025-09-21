39b3477e4ac3403752b00200b6da84afb6d49e02
Mr. EnglishCat <mr.englishcat@gmail.com>
2025-09-21 19:27:37 +0300

    Добавлено
    , help_text=''

--- auth_app/models/roles.py
+++ auth_app/models/roles.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class RolesModel(models.Model):
-    name = models.CharField('Имя роли', max_length=100, unique=True)
-    description = models.TextField('Описание роли', null=True, blank=True)
+    name = models.CharField('Название роли', max_length=100, unique=True, help_text='Название роли.')
+    code_name = models.CharField('Код роли', max_length=100, unique=True, help_text='Кодовое название на английском, используется для определения прав в коде.')
+    description = models.TextField('Описание роли', null=True, blank=True, help_text='Описание роли, к примеру что делает или для кого.')
     permissions = models.ManyToManyField('auth_app.PermissionsModel', related_name='roles', blank=True,
-                                         verbose_name='Список ролей')
-    user = models.ManyToManyField('auth_app.CustomUserModel', related_name='users')
+                                         verbose_name='Список ролей', help_text='Связь со списком ролей и ресурсами.')
+    users = models.ManyToManyField('auth_app.CustomUserModel', related_name='roles', help_text='Поле для связи роли с пользователями и конкретным пользователем.')
 
     class Meta:
         verbose_name = 'Роль'
