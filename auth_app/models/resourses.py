644a231f9a903d048107d535876383d62ab9d673
Mr. EnglishCat <mr.englishcat@gmail.com>
2025-09-23 12:12:26 +0300

    Редактирование README.md
    Дописал описание. Редактирование таблиц.
    
    Класс удален за ненадобностью

--- auth_app/models/resourses.py
+++ auth_app/models/resourses.py
@@ -3,7 +3,7 @@ from django.db import models
 class ResourcesModel(models.Model):
 
     name = models.CharField('Название ресурса', max_length=100, unique=True, help_text='Название ресурса.')
-    code_name = models.CharField('Код ресурса', default='', max_length=100, unique=True, help_text='Кодовое название ресурса к которому нужен доступ')
+    code_name = models.CharField('Код ресурса', max_length=100, unique=True, help_text='Кодовое название ресурса к которому нужен доступ')
     description = models.TextField('Описание ресурса', null=True, blank=True, help_text='Описание ресурса.')
 
     class Meta:
