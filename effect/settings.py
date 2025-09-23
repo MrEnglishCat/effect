3acb6eaa648693ee5ca9e2adb7a4c220021c0237
Mr. EnglishCat <mr.englishcat@gmail.com>
2025-09-23 11:33:24 +0300

    Добавлены
    вчера, забыл, про то что БД должна была быть Postgres. Добавил в сеттингс настройки для постгреса, поставил модуль psycopg2.
    
    По дефолту название бд effect
    user postgres
    password postgres
    port 5432

--- effect/settings.py
+++ effect/settings.py
@@ -85,10 +85,23 @@ WSGI_APPLICATION = 'effect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+# Для тестов SqLite
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'effect',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
